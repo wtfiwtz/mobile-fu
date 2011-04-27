062ef974242e5dae1a431b9fcce0cc4e49098e98
Ben Langfeld <ben@langfeld.me>
2011-04-27 12:38:21 +0100

    Some work on the way to making this a gem

---
+++ lib/mobile-fu/version.rb
@@ -0,0 +1,3 @@
+module MobileFu
+  VERSION = "0.0.1"
+end
--- spec/mobilized_styles_spec.rb
+++ spec/mobilized_styles_spec.rb
@@ -1,5 +1,4 @@
-
-require File.dirname(__FILE__) + '/spec_helper'
+require 'spec_helper'
 
 module ActionView
   module Helpers
@@ -9,27 +8,27 @@ module ActionView
   end
 end
 
-describe MobilizedStyles do
-  
+describe MobileFu::MobilizedStyles do
+
   before(:each) do
     @view = mock(:ActionView)
     @request = mock(:ActionRequest)
-    @view.extend(MobilizedStyles)
+    @view.extend(MobileFu::MobilizedStyles)
     @view.stub!(:request).and_return(@request)
     @request.stub!(:user_agent)
   end
-  
+
   def ua(str)
     @request.stub!(:user_agent).and_return(str)
   end
-  
+
   it "will include a mobilized css file if it recognizes a string in the user agent" do
     ua "iphone"
     File.should_receive(:exist?).with("stylesheets/style_iphone.css").and_return(true)
     @view.should_receive(:stylesheet_link_tag_without_mobilization).with("style", "style_iphone")
     @view.stylesheet_link_tag_with_mobilization("style")
   end
-  
+
   it "includes mobiziled css files whether or not the original call to stylesheet_link_tag used a file extension" do
     ua "blackberry"
     File.should_receive(:exist?).with("stylesheets/style_blackberry.css").and_return(true)
