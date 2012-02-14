6e91e94888aa6f81b00b181757fc7e224503e23e
Mohd Zaid <atuk84@hotmail.com>
2012-02-15 07:22:57 +0800

    fixed #9 #11 removed mobile user agent and fixed readme

--- lib/mobile-fu.rb
+++ lib/mobile-fu.rb
@@ -31,18 +31,9 @@ end
 
 module ActionController
   module MobileFu
-    # These are various strings that can be found in mobile devices.  Please feel free
-        # to add on to this list.
-        MOBILE_USER_AGENTS =  'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
-                              'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
-                              'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
-                              'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
-                              'webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|' +
-                              'mobile'
-
     # These are various strings that can be found in tablet devices.  Please feel free
     # to add on to this list.
-    TABLET_USER_AGENTS =  'ipad|android 3.0|xoom|sch-i800|playbook|tablet|kindle'
+    TABLET_USER_AGENTS =  'ipad|android 3.0|xoom|sch-i800|playbook|tablet|kindle|honeycomb'
 
     def self.included(base)
       base.extend ClassMethods
