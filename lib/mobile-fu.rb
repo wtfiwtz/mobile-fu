3618b82e5e61d5fe861478144b413889f53c642e
Zaid M. Said <sentul@brainstorm.pro>
2012-07-09 19:58:46 +0800

    added html fallback if no mobile template are found

--- lib/mobile-fu.rb
+++ lib/mobile-fu.rb
@@ -168,6 +168,42 @@ module ActionController
   end
 end
 
+module ActionView
+  class PathSet < Array
+
+    def find_with_default_template(path, prefix = nil, partial = false, details = {}, key = nil)
+      if prefix == "layouts"
+        # Layouts have their own way of managing fallback, better leave them alone
+        find_without_default_template(path, prefix, partial, details, key)
+      else
+        begin
+          find_without_default_template(path, prefix, partial, details, key)
+        rescue MissingTemplate => e
+          raise e if details[:formats] == [:html]
+          html_details = details.dup.merge(:formats => [:html])
+          find_without_default_template(path, prefix, partial, html_details, key)
+        end
+      end
+    end
+    alias_method_chain :find, :default_template
+
+  end
+  
+  class Resolver
+    
+    def cached(key, prefix, name, partial)
+      return yield unless key && caching?
+      cache_content = yield
+      if cache_content.empty?
+        []
+      else
+        @cached[key][prefix][name][partial] ||= cache_content
+      end
+    end
+    
+  end
+end
+
 if Rails::VERSION::MAJOR < 3
   ActionController::Base.send :include, ActionController::MobileFu
   ActionView::Base.send :include, MobileFu::Helper
